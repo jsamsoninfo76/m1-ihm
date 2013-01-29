9a580b6005e7890ad3ccf44f18af1b77a1510471
Jeremie.Samson76@gmail.com <Jeremie.Samson76@gmail.com@4431e0f4-430a-bc32-03f3-9cc705dac4cc>
2013-01-29 14:29:05 +0000

    Q1

---
+++ branches/tp2/Q1_Incrementer.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+/*
+ * Question 1. 
+ * En utilisant la classe JFrame, le gestionnaire de placement BoxLayout, les classes JLabel et JButton, réalisez une interface similaire à celle présentée figure 1 
+ * 
+ */
+public class Q1_Incrementer extends JFrame{
+
+	public Q1_Incrementer(){
+		
+	}
+}
