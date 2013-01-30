7406db15c79766cba82a30430a4d8529fd30d432
Jeremie.Samson76@gmail.com <Jeremie.Samson76@gmail.com@4431e0f4-430a-bc32-03f3-9cc705dac4cc>
2013-01-30 13:22:24 +0000

    TP2 termine

--- src/Q9_SelectCouleur.java
+++ src/Q9_SelectCouleur.java
@@ -1,7 +1,6 @@
-import java.awt.Container;
+import java.awt.Color;
 import java.awt.GridLayout;
 
-import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
@@ -20,11 +19,11 @@ public class Q9_SelectCouleur extends JFrame implements ChangeListener{
 	private JSlider slideR, slideG, slideB;
 	private JTextField tfR, tfG, tfB, tfHexa;
 	private JPanel panColor;
-	
+	private Color c;
 	private static final long serialVersionUID = 1L;
 	
 	public Q9_SelectCouleur(){
-	    this.setTitle("Q1_Incrementer");
+	    this.setTitle("Q9_SelectCouleur");
 	    this.setSize(200, 200);
 	    this.setLocationRelativeTo(null);
 	    this.setLayout(new GridLayout(1,3));
@@ -36,11 +35,11 @@ public class Q9_SelectCouleur extends JFrame implements ChangeListener{
 	public void initCompo(){
 		JPanel slides = new JPanel();
 			slides.setLayout(new GridLayout(3,1));
-			slideR = new JSlider();
+			slideR = new JSlider(0,255,0);
 			slideR.addChangeListener(this);
-			slideG = new JSlider();
+			slideG = new JSlider(0,255,0);
 			slideG.addChangeListener(this);
-			slideB = new JSlider();
+			slideB = new JSlider(0,255,0);
 			slideB.addChangeListener(this);
 			slides.add(slideR);
 			slides.add(slideG);
@@ -48,17 +47,19 @@ public class Q9_SelectCouleur extends JFrame implements ChangeListener{
 		
 		JPanel slidesValues = new JPanel();
 			slidesValues.setLayout(new GridLayout(3,1));
-			tfR = new JTextField();
-			tfG = new JTextField();
-			tfB = new JTextField();
+			tfR = new JTextField(String.valueOf(slideR.getValue()));
+			tfG = new JTextField(String.valueOf(slideG.getValue()));
+			tfB = new JTextField(String.valueOf(slideB.getValue()));
 			slidesValues.add(tfR);
 			slidesValues.add(tfG);
 			slidesValues.add(tfB);
 		
 		JPanel hexaColor = new JPanel();
 			hexaColor.setLayout(new GridLayout(2,1));
-			tfHexa = new JTextField();
+			c = new Color(Integer.valueOf(tfR.getText()),Integer.valueOf(tfG.getText()),Integer.valueOf(tfB.getText()));;
+			tfHexa = new JTextField(Integer.toHexString( c.getRGB() & 0x00ffffff ));
 			panColor = new JPanel();
+			panColor.setBackground(c);
 			hexaColor.add(tfHexa);
 			hexaColor.add(panColor);
 			
@@ -68,8 +69,18 @@ public class Q9_SelectCouleur extends JFrame implements ChangeListener{
 	}
 	
 	public void stateChanged(ChangeEvent arg0) {
-		// TODO Auto-generated method stub
+		JSlider src = (JSlider)arg0.getSource();
+		JTextField tf;
 		
+		if (src  == slideR) tf = tfR;
+		else if (src == slideG) tf = tfG;
+		else tf = tfB;
+			
+		tf.setText(String.valueOf(src.getValue()));
+		
+		Color c = new Color(Integer.valueOf(tfR.getText()),Integer.valueOf(tfG.getText()),Integer.valueOf(tfB.getText()));;
+	    tfHexa.setText(Integer.toHexString( c.getRGB() & 0x00ffffff ));
+	    panColor.setBackground(c);
 	}
 	
 	public static void main(String args[]){
