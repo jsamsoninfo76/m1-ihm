73f5106ea34f48d4075035515a880f921102d5b9
Jeremie.Samson76@gmail.com <Jeremie.Samson76@gmail.com@4431e0f4-430a-bc32-03f3-9cc705dac4cc>
2013-02-05 14:42:30 +0000

    import temperatureMVC

---
+++ branches/tp3/TemperatureMVC.java
@@ -0,0 +1,29 @@
+/**
+ * @author <a href="mailto:gery.casiez@lifl.fr">Gery Casiez</a>
+ */
+
+import model.*;
+import view.*;
+import controller.*;
+
+public class TemperatureMVC {
+	public TemperatureMVC() {
+		TemperatureModel tempmod = new TemperatureModel();
+		TemperatureController tempcontrolC = new TemperatureController(tempmod);
+		TemperatureController tempcontrolF = new TemperatureController(tempmod);
+		TemperatureVueCelsuis pvc = new TemperatureVueCelsuis(tempmod, tempcontrolC, 100, 200);
+		TemperatureVueFarenheit tvf = new TemperatureVueFarenheit(tempmod, tempcontrolF, 100, 350);
+		tempcontrolC.addView(pvc);
+		tempcontrolF.addView(tvf);	
+	}
+	
+	public static void main(String args[]) {
+		//Schedule a job for the event-dispatching thread:
+    //creating and showing this application's GUI.
+    javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new TemperatureMVC();
+      }
+    });
+	}
+}
